4c2f9b74ada97c30dfaad6a9fef5670144c11ef6
Frost <notshivamsingh@gmail.com>
2026-03-17 22:06:49 +0530

    feat: create Message Mongoose model and add PORT to .env

--- chat-app-backend/models/Message.model.js
+++ chat-app-backend/models/Message.model.js
@@ -1,7 +1,23 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-    
+
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+
+    },
+    groupId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Group",
+        required:true
+    },
+      text:{
+        type:String,
+        required:true
+    }
+
 },{timestamps:true})
 
 export const Message = mongoose.model("Message",messageSchema)
