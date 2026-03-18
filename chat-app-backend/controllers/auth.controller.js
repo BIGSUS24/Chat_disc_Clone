fde5090617c0291333307e74be905f9d7a09f029
Frost <notshivamsingh@gmail.com>
2026-03-18 12:53:04 +0530

    feat: Implement initial user signup functionality with API response and async error handling utilities.

--- chat-app-backend/controllers/auth.controller.js
+++ chat-app-backend/controllers/auth.controller.js
@@ -1,12 +1,17 @@
 import {User} from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiResponse} from "../utils/ApiResponce.js";
 
-export const signup = async (req,res)=>{
-    try {
-        const {username,password,email} = req.body;
-        
-    } catch (error) {
-        
+export const signup = asyncHandler(async (req,res) => {
+    const {username,password,email} = req.body;
+    
+    // validation basic
+    if ([!username,!email,!password].some((field)=>field?.trim() === "")) {
+        throw new ApiError(400,"Fill All the Fields these are required")
+    
     }
-}
+    
+    
+})
