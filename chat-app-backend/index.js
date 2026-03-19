464296e1a252f8c54222f4a28ecb88add251610e
Frost <notshivamsingh@gmail.com>
2026-03-19 16:21:45 +0530

    feat: initialize Express server with MongoDB connection, API routes, and global error handling

--- chat-app-backend/index.js
+++ chat-app-backend/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import groupRoutes from "./routes/group.routes.js";
+
 
 dotenv.config();
 const app = express();
@@ -11,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/groups:",groupRoutes)
+app.use("/api/v1/groups",groupRoutes);
 
 app.get("/",(req,res)=>{
   res.send("Server is running")
