baf84ba11405e634af7f4b2cc10a3b5cd8476a22
Frost <notshivamsingh@gmail.com>
2026-03-20 12:15:59 +0530

    feat: add message routes for sending and retrieving messages, protected by JWT authentication.

--- chat-app-backend/routes/message.routes.js
+++ chat-app-backend/routes/message.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { getMessages } from "../controllers/message.controller.js";
 import { sendMessage } from "../controllers/message.controller.js";
-import {verifyJwT} from "../middlewares/auth.middleware.js"
+import {verifyJWT} from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 
-router.route("/:groupId").post(verifyJwT,sendMessage);
-router.route("/;groupId").get(verifyJwT,getMessages)
+router.route("/:groupId").post(verifyJWT,sendMessage);
+router.route("/:groupId").get(verifyJWT,getMessages);
 
 export default router;
