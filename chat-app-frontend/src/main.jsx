e36d1f8279d997e8d59e411555e5935ae752bd19
Frost <notshivamsingh@gmail.com>
2026-03-21 22:14:55 +0530

    feat: add initial login page and configure application with routing and authentication context.

--- chat-app-frontend/src/main.jsx
+++ chat-app-frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
+import { AuthContext } from './context/AuthContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <BrowserRouter>
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>
+  </BrowserRouter>
+   
+  </React.StrictMode>,
 )
